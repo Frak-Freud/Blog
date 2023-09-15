6d105f22c7025dec596ac5cc5c7f0debb8f02326
root <2834213547@qq.com>
2023-09-15 12:52:47 +0800

    初始化提交

---
+++ src/main/java/com/blog/interceptor/AdminLoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.blog.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 后台系统身份验证拦截器
+ */
+
+@Component
+public class AdminLoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestServletPath = request.getServletPath();
+        if (requestServletPath.startsWith("/admin") && request.getSession().getAttribute("loginUser") == null) {
+            request.getSession().setAttribute("errorMsg", "请重新登录");
+            response.sendRedirect(request.getContextPath() + "/admin/login");
+            return false;
+        } else {
+            request.getSession().removeAttribute("errorMsg");
+            return true;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+}
