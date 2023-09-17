92d9a25706ee97f45bdeb7ff451365936f353b40
root <2834213547@qq.com>
2023-09-17 20:26:26 +0800

    添加编辑标签功能

--- src/main/java/com/blog/controller/admin/AdminController.java
+++ src/main/java/com/blog/controller/admin/AdminController.java
@@ -93,9 +93,8 @@ public class AdminController {
     public String passwordUpdate(HttpServletRequest request,
                                  @RequestParam("originalPassword") String originalPassword,
                                  @RequestParam("newPassword") String newPassword){
-        if(!StringUtils.hasText(originalPassword) || !StringUtils.hasText(newPassword)){
+        if(!StringUtils.hasText(originalPassword) || !StringUtils.hasText(newPassword))
             return "参数不能为空";
-        }
         Integer loginUserId = (Integer) request.getSession().getAttribute("loginUserId");
         if(adminUserService.updatePassword(loginUserId, originalPassword, newPassword)){
             //修改成功后清空session中的数据，前端控制跳转至登录页
